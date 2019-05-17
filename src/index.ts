9719e249c2a341f7b2778bf9212021a513fafb77
Rich Harris <richard.a.harris@gmail.com>
2019-05-17 13:11:34 -0400

    implementation from wikipedia

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,85 @@
-import { Curve, CurveOptions } from './Curve';
-import { KeyframeData } from './Keyframe';
+export default function keyframes(frames: Array<[number, number]>) {
+	frames = frames.slice().sort((a, b) => a[0] - b[0]);
 
-export default function keyframes(frames: KeyframeData[], opts?: CurveOptions) {
-	return new Curve(frames, opts);
+	// Get consecutive differences and slopes
+	const dxs = [];
+	const dys = [];
+	const ms = [];
+
+	for (let i = 0; i < frames.length - 1; i += 1) {
+		const a = frames[i];
+		const b = frames[i + 1];
+
+		dxs[i] = b[0] - a[0];
+		dys[i] = b[1] - a[1];
+		ms[i] = dys[i] / dxs[i];
+	}
+
+	// Get degree-1 coefficients
+	const c1s = [ms[0]];
+	for (let i = 0; i < dxs.length - 1; i++) {
+		const m = ms[i];
+		const m_next = ms[i + 1];
+
+		if (m * m_next <= 0) {
+			c1s[i] = 0;
+		} else {
+			const dx = dxs[i];
+			const dx_next = dxs[i + 1];
+			const common = dx + dx_next;
+
+			c1s[i] = 3 * common / ((common + dx_next) / m + (common + dx) / m_next);
+		}
+	}
+
+	c1s.push(ms[ms.length - 1]);
+
+	// Get degree-2 and degree-3 coefficients
+	const c2s: number[] = [];
+	const c3s: number[] = [];
+
+	for (let i = 0; i < c1s.length - 1; i++) {
+		const c1 = c1s[i];
+		const m = ms[i];
+		const inv_dx = 1/dxs[i];
+		const common = c1 + c1s[i + 1] - m - m;
+
+		c2s[i] = (m - c1 - common) * inv_dx;
+		c3s[i] = common * inv_dx * inv_dx;
+	}
+
+	const first = frames[0];
+	const last = frames[frames.length - 1];
+
+	return (x: number) => {
+		// clamp
+		if (x <= first[0]) return first[1];
+		if (x >= last[0]) return last[1];
+
+		// Search for the interval x is in, returning the corresponding y if x is one of the original xs
+		let low = 0;
+		let high = c3s.length - 1;
+
+		while (low <= high) {
+			const mid = Math.floor(0.5 * (low + high));
+
+			var x_here = frames[mid][0];
+
+			if (x_here < x) {
+				low = mid + 1;
+			} else if (x_here > x) {
+				high = mid - 1;
+			} else {
+				return frames[mid][1];
+			}
+		}
+
+		const i = Math.max(0, high);
+
+		// Interpolate
+		const diff = x - frames[i][0];
+		const diff_sq = diff * diff;
+
+		return frames[i][1] + c1s[i] * diff + c2s[i] * diff_sq + c3s[i] * diff * diff_sq;
+	};
 }
--- test/test.ts
+++ test/test.ts
@@ -5,35 +5,35 @@ require('console-group').install();
 
 describe('smooth-keyframes', () => {
 	it('returns an exact value', () => {
-		const curve = keyframes([
-			{ time: 0, value: 10 },
-			{ time: 1, value: 20 },
-			{ time: 2, value: 30 }
+		const y = keyframes([
+			[0, 10],
+			[1, 20],
+			[2, 30]
 		]);
 
 
-		assert.equal(curve.at(1), 20);
+		assert.equal(y(1), 20);
 	});
 
 	it('returns an intermedia value', () => {
-		const curve = keyframes([
-			{ time: 0, value: 0 },
-			{ time: 1, value: 100 },
-			{ time: 2, value: 50 }
+		const y = keyframes([
+			[0, 0],
+			[1, 100],
+			[2, 50]
 		]);
 
-		const v = curve.at(0.5);
+		const v = y(0.5);
 		assert.ok(0 < v && v < 100);
 	});
 
 	it('clamps values', () => {
-		const curve = keyframes([
-			{ time: 0, value: 10 },
-			{ time: 1, value: 20 },
-			{ time: 2, value: 30 }
+		const y = keyframes([
+			[0, 10],
+			[1, 20],
+			[2, 30]
 		]);
 
-		assert.equal(curve.at(-1), 10);
-		assert.equal(curve.at(3), 30);
+		assert.equal(y(-1), 10);
+		assert.equal(y(3), 30);
 	});
 });
